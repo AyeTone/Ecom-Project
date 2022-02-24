6128f97522a59a106eef48a10306ae283ad52703
Troy Richards <94700828+AyeTone@users.noreply.github.com>
2022-02-24 11:13:09 -0500

    ProductList Page Created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { hover } from "@testing-library/user-event/dist/hover";
 import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
 
 function App() {
-  return <Home />
+  return <ProductList />
 }
 
 export default App;
