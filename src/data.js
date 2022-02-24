03e179fb75dcd1198283b27d5c9c97689bdf42f9
Troy Richards <94700828+AyeTone@users.noreply.github.com>
2022-02-24 08:04:06 -0500

    Added Images to JSON file

--- src/data.js
+++ src/data.js
@@ -2,20 +2,24 @@ export const sliderItems = [
     {
         id: 1,
         title: "Spring Sale", 
-        img: 'data:image:/png;base64,img_1',
+        img: "https://images.unsplash.com/photo-1529139574466-a303027c1d8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
         desc: "Don't compromise on style! Get Flat 30% off for new arrivals",
         bg: "f5fafd"
     },
     {
         id: 2,
         title: "Summer Collection",
+        img: 'https://images.unsplash.com/photo-1572804013309-59a88b7e92f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=746&q=80',
         desc: "Get in early! Get up to 50% off on summer wear for a limited time!",
         bg: "fcf1ed"
     },
     {
         id: 3,
         title: "Valentine Sale",
+        img: 'https://images.unsplash.com/photo-1504051771394-dd2e66b2e08f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
         desc: "Get something special for that special someone! Up to 40% off our Valentine's Day Collection",
         bg: "fbf0f4"
     }
 ]
+
+
