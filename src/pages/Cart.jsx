ed92288e3c651060cc442bcfbcc61d5c492c61cc
Troy Richards <94700828+AyeTone@users.noreply.github.com>
2022-02-24 16:23:36 -0500

    Cart Page Design Complete

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -70,6 +70,7 @@ const ProductColor = styled.div`
   background-color: ${props => props.color};
 `
 const ProductSize = styled.span``
+
 const PriceDetail = styled.div`
   flex: 1;
   display: flex;
@@ -100,6 +101,35 @@ const Hr = styled.hr `
 
 const Summary = styled.div`
   flex: 1;
+  border: 0.5px solid gray;
+  border-radius: 5px;
+  padding: 20px;
+  height: 50vh;
+`
+
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+  text-transform: uppercase;
+`
+const SummaryItem = styled.div`
+  margin: 30px 0;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${props => props.type === "total" && "500"};
+  font-size: ${props => props.type === "total" && "24px"};
+`
+const SummaryItemText= styled.span``
+
+const SummaryItemPrice = styled.span``
+
+const Button = styled.button`
+  width: 100%;
+  padding: 10px 15px;
+  background-color: black;
+  color: white;
+  border-radius: 5px;
+  text-transform: uppercase;
+  font-weight: bold;
 `
 
 const Cart = () => {
@@ -156,11 +186,30 @@ const Cart = () => {
                   <ProductAmount>2</ProductAmount>
                   <Remove />
                 </ProductAmountContainer>
-                <ProductPrice>$ 135</ProductPrice>
+                <ProductPrice>$ 40</ProductPrice>
               </PriceDetail>
             </Product>
           </Info>
-          <Summary>SUmmary</Summary>
+          <Summary>
+            <SummaryTitle>Order Summary</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText> SubTotal</SummaryItemText>
+              <SummaryItemPrice>$ 175 </SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText> Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>$ 2.50 </SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText> Shipping Discount </SummaryItemText>
+              <SummaryItemPrice>$ -2.50 </SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText > Total </SummaryItemText>
+              <SummaryItemPrice>$ 170 </SummaryItemPrice>
+            </SummaryItem>
+            <Button>Checkout Now</Button>
+          </Summary>
         </Bottom>
       </Wrapper>
       <Footer />
